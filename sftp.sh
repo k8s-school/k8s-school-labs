8847dabaa390cfb920da5796f01ff03b8dfb5846
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-05-22 23:20:25 +0200

    Update current feature

--- sftp.sh
+++ sftp.sh
@@ -8,11 +8,6 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 SERVER_DIR="www/resources"
 LOCAL_DIR="$DIR/public"
 
-"$DIR"/content/CKAD-Exercices/generate.sh
-
-rm -rf "$LOCAL_DIR/*"
-hugo
-
 . "$DIR/env-creds.sh"
 
 #yafc fish://"$SERVER_USER"@"$SERVER"
